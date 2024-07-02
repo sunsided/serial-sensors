0f86870c13ad9d0e6d9994d583e63d1c21e0f0d5
Markus Mayer <widemeadows@gmail.com>
2024-07-02 15:33:56 +0200

    Add initial data send and receive

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io::{self, Write};
 use std::time::Duration;
-use tokio::io::AsyncReadExt;
+use tokio::io::{self, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 use tokio_serial::{DataBits, FlowControl, Parity, SerialPortBuilderExt, SerialStream, StopBits};
 
 #[tokio::main]
@@ -18,32 +18,54 @@ async fn main() -> anyhow::Result<()> {
         .open_native_async()
         .expect("Failed to open port");
 
+    let (serial_in, mut receiver) = unbounded_channel::<String>();
+    let (command, serial_out) = unbounded_channel::<String>();
+
     // Spawn a thread for reading data from the serial port
-    tokio::spawn(handle_data_recv(port));
+    tokio::spawn(handle_data_recv(port, serial_in, serial_out));
 
-    /*
-    // Main loop for sending commands to the serial port
+    // Spawn a task for reading from stdin and sending commands
+    tokio::spawn(handle_std_input(command));
+
+    // Main loop for printing input from the serial line.
     loop {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
-        port.write_all(input.as_bytes())?;
-        port.flush()?;
+        if let Some(data) = receiver.recv().await {
+            print!("{}", data);
+        }
     }
-     */
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-    Ok(())
 }
 
-async fn handle_data_recv(mut port: SerialStream) {
+async fn handle_std_input(command: UnboundedSender<String>) {
+    let stdin = io::stdin();
+    let reader = BufReader::new(stdin);
+    let mut lines = reader.lines();
+
+    while let Some(line) = lines.next_line().await.unwrap_or(None) {
+        let line = line.trim().to_string();
+        if !line.is_empty() {
+            command.send(line).unwrap();
+        }
+    }
+}
+
+async fn handle_data_recv(
+    mut port: SerialStream,
+    serial_in: UnboundedSender<String>,
+    mut serial_out: UnboundedReceiver<String>,
+) -> anyhow::Result<()> {
     let mut buf: Vec<u8> = vec![0; 1024];
     loop {
+        // Send data when serial_out has a message
+        if let Ok(command) = serial_out.try_recv() {
+            port.write_all(command.as_bytes()).await?;
+        }
+
         match port.read(&mut buf).await {
             Ok(bytes_read) => {
                 if bytes_read > 0 {
-                    print!("{}", String::from_utf8_lossy(&buf[..bytes_read]));
-                    io::stdout().flush().unwrap();
+                    let data = String::from_utf8_lossy(&buf[..bytes_read]).into_owned();
+                    serial_in.send(data)?;
+                    io::stdout().flush().await?;
                 }
             }
             Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
