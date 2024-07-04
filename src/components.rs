aa7113350b706ebb49a50bda7ac49b487f7bc453
Markus Mayer <widemeadows@gmail.com>
2024-07-04 14:41:35 +0200

    Remove demo home component

--- src/components.rs
+++ src/components.rs
@@ -1,13 +1,12 @@
 use color_eyre::eyre::Result;
 use crossterm::event::{KeyEvent, MouseEvent};
-use ratatui::layout::Rect;
 use ratatui::Frame;
+use ratatui::layout::Rect;
 use tokio::sync::mpsc::UnboundedSender;
 
 use crate::{action::Action, config::Config, tui::Event};
 
 pub mod fps;
-pub mod home;
 pub mod streaming;
 
 /// A TUI component.
